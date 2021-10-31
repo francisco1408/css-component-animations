fcdb290fa3868e99596182513c5c2009ad78ec08
Francisco Valdes <fco1408@outlook.com>
2021-10-31 13:35:18 -0600

    Modified transition to first and second sections and made many modifications to the fire animation

--- main.js
+++ main.js
@@ -5,11 +5,23 @@ console.log(getSections);
 let setAnimations = (sectionBox)=>{
     getSections.forEach((sectionBox)=>{
         let rect = sectionBox.getBoundingClientRect();
-        if(rect.bottom <= window.innerHeight*1.8 && rect.top >= window.innerHeight*-0.8){
-            sectionBox.classList.add("animated");
-        } else {
+        if(sectionBox.id=="fire"){
             sectionBox.classList.remove("animated");
+            if(rect.bottom <= window.innerHeight*1.85 && rect.top >= window.innerHeight*-0.85){
+                sectionBox.classList.add("animated_fire");
+            } else {
+                sectionBox.classList.remove("animated_fire");
+                
+            }
+        } else {
+            sectionBox.classList.remove("animated_fire");
+            if(rect.bottom <= window.innerHeight*1.85 && rect.top >= window.innerHeight*-0.85){
+                sectionBox.classList.add("animated");
+            } else {
+                sectionBox.classList.remove("animated");
+            }
         }
+        
     });
 };
 setAnimations();

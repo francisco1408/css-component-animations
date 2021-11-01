0ced799fb544dde57428f029285e954d26967b4b
Francisco Valdes <fco1408@outlook.com>
2021-10-31 22:26:13 -0600

    Added transition for thunder section

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
 console.log(2+2);
 let getSections = document.querySelectorAll("section");
 console.log(getSections);
-
+let isVisible = (rect) =>rect.bottom <= window.innerHeight*1.85 && rect.top >= window.innerHeight*-0.85;
 let setAnimations = (sectionBox)=>{
     getSections.forEach((sectionBox)=>{
         let rect = sectionBox.getBoundingClientRect();
         if(sectionBox.id=="fire"){
             sectionBox.classList.remove("animated");
-            if(rect.bottom <= window.innerHeight*1.85 && rect.top >= window.innerHeight*-0.85){
+            sectionBox.classList.remove("animated_thunder");
+            if(isVisible(rect)){
                 sectionBox.classList.add("animated_fire");
             } else {
                 sectionBox.classList.remove("animated_fire");
                 
             }
-        } else {
+        } else if(sectionBox.id=="thunder"){
+            sectionBox.classList.remove("animated");
             sectionBox.classList.remove("animated_fire");
-            if(rect.bottom <= window.innerHeight*1.85 && rect.top >= window.innerHeight*-0.85){
+            if(isVisible(rect)){
+                sectionBox.classList.add("animated_thunder");
+            } else {
+                sectionBox.classList.remove("animated_thunder");
+                
+            }
+        }
+        else {
+            sectionBox.classList.remove("animated_fire");
+            sectionBox.classList.remove("animated_thunder");
+            if(isVisible(rect)){
                 sectionBox.classList.add("animated");
             } else {
                 sectionBox.classList.remove("animated");

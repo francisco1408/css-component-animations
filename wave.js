e72605039d051935747cdb65d1c364184ef56503
Francisco Valdes <fco1408@outlook.com>
2021-11-11 23:25:28 -0600

    added watter ripple effect on giyus card

---
+++ wave.js
@@ -0,0 +1,38 @@
+let wave_container = document.querySelector(".wave_container");
+
+let handleWave = (event)=> {
+    
+    let waves = document.querySelectorAll(".wavywave");
+    if(waves.length < 6){
+        console.log("------");
+        let cord = document.querySelector(".wave_container").getBoundingClientRect();
+        console.log(cord.top);
+        console.log(cord.left);
+        console.log("--");
+        console.log(event.clientY);
+        console.log(event.clientX);
+        let waveid = "wave" + (waves.length).toString();
+        let death_wave = document.createElement("div");
+        death_wave.id = waveid;
+        death_wave.classList.add("wavywave");
+        death_wave.classList.add("main");
+        death_wave.innerHTML = `
+            <div class="wave wave1"></div>
+            <div class="wave wave2"></div>
+            <div class="wave wave3"></div>`;
+        wave_container.appendChild(death_wave);
+        let half = 205;
+        death_wave.style.display = "absolute";
+        death_wave.style.top = (event.clientY - half).toString() + "px";
+        death_wave.style.left = (event.clientX - half).toString() + "px";
+        setTimeout(()=>{ 
+            let death_wave = document.getElementById(waveid);
+            death_wave.parentNode. removeChild(death_wave);
+        }, 4000);
+    } else {
+        console.log("No more");
+    }
+
+  }
+  
+  wave_container.addEventListener("click", handleWave);
